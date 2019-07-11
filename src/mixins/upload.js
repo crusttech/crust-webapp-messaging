0ace7316c8eeb446a719691c973ab83a04a8dc3d
Denis Arh <denis.arh@kendu.si>
2019-07-11 19:41:10 +0200

    Import corteza-webapp-messaging as a package and extend corteza-base theme

--- src/mixins/upload.js
+++
@@ -1,9 +0,0 @@
-export default {
-  methods: {
-    // Allow only if it is a 'File'
-    handleShow (e, allow = () => {}) {
-      if (e.dataTransfer.types.indexOf('Files') < 0) return
-      allow()
-    },
-  },
-}
