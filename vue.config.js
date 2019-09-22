9e7133b9d3293656c3c8304e02c801ed4a53512a
Jože Fortun <joze.fortun@gmail.com>
2019-09-22 22:03:26 +0200

    Add appFlavour to build variables

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const buildVueConfig = require('corteza-webapp-messaging/vue.config-builder')
 
 module.exports = buildVueConfig({
+  appFlavour: 'Crust',
   appName: 'messaging',
   appLabel: 'Crust Messaging',
   theme: 'crust-base',
